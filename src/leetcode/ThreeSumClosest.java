304083564ee16b5d07727f1486dffd960a70a5ba
aajmera <Letsdoit$21>
2021-09-21 10:52:33 +0530

    three sum closest

--- src/leetcode/ThreeSumClosest.java
+++ src/leetcode/ThreeSumClosest.java
@@ -10,7 +10,16 @@ public class ThreeSumClosest {
         //sort the array first
         Arrays.sort(nums);
 
+        //take the ith element
         for(int i=0; i<nums.length-2; i++) {
+            /*
+             * Iterate the array such that
+             * - take the first and the last element after the ith element
+             * - calculate the sum for these three values
+             * - check if difference is lesser than previously found difference
+             * - now see if sum < target, then pick next element of j
+             * - else if sum > target, then pick previous element of k
+             */
             for(int j=(i+1), k=nums.length-1; j < k; ) {
                 int sum = nums[i] + nums[j] + nums[k];
                 int tempDiff = Math.abs(target-sum);
